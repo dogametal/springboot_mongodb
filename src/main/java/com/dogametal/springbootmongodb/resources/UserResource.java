d02d27fc3db15ecfa26e2600b3238190ba0b4aa0
dogametal <dogametal@hotmail.com>
2020-11-21 05:45:06 -0300

    Using Default DTO returning user

--- src/main/java/com/dogametal/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/dogametal/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.dogametal.springbootmongodb.resources;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dogametal.springbootmongodb.domain.User;
+import com.dogametal.springbootmongodb.dto.UserDTO;
 import com.dogametal.springbootmongodb.services.UserService;
 
 @RestController
@@ -20,9 +23,11 @@ public class UserResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET) // Another way to use this request @GetMapping 	
-	public ResponseEntity <List<User>> findAll(){
-		List<User> list = service.findAll();	
-		return ResponseEntity.ok().body(list); //Another to do the same - return ResponseEntity.ok(list);
+	public ResponseEntity <List<UserDTO>> findAll(){
+		List<User> list = service.findAll();
+		//Method to tranform each item from list to ListDTO using lambda with collectors
+		List<UserDTO> listDto = list.stream().map(item -> new UserDTO(item)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto); //Another to do the same - return ResponseEntity.ok(list);
 
 	}
 }
