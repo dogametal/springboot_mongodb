ec00b3f108b48c78ccb88daf9307fddd69dc9044
dogametal <dogametal@hotmail.com>
2020-11-25 05:20:19 -0300

    User deletion by DELETE

--- src/main/java/com/dogametal/springbootmongodb/services/UserService.java
+++ src/main/java/com/dogametal/springbootmongodb/services/UserService.java
@@ -31,6 +31,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete (String id) {
+		//Case id not exists throw exception
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 		
