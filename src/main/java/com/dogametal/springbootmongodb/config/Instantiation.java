6892f5fa333edf31c66bfe6c2310fa081d50d963
dogametal <dogametal@hotmail.com>
2020-11-26 05:40:54 -0300

    Created Post with ann User

--- src/main/java/com/dogametal/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/dogametal/springbootmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.dogametal.springbootmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.dogametal.springbootmongodb.domain.Post;
 import com.dogametal.springbootmongodb.domain.User;
+import com.dogametal.springbootmongodb.repository.PostRepository;
 import com.dogametal.springbootmongodb.repository.UserRepository;
 
 @Configuration
@@ -15,8 +19,14 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -25,6 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User douglas = new User(null, "Douglas Silva", "douglas.it.soares@gmail.com");
 		User daniel = new User(null, "Daniel Soares", "daniel.bebe.soares@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob, douglas, daniel));
+		
+		postRepository.deleteAll();
+		Post post1 = new Post(null,sdf.parse("26/11/2020"), "Partiu nascimento", "Seja bem vindo, filho !", douglas);
+		Post post2 = new Post(null,sdf.parse("27/11/2020"), "Nasceu  !!!", "Papai ama Daniel ", douglas);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
 	
