e5c977d0b41ba33ceae004386a84788abb36b499
dogametal <dogametal@hotmail.com>
2020-11-30 05:52:37 -0300

    Query Simple with MongoDB Repository

--- src/main/java/com/dogametal/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/dogametal/springbootmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.dogametal.springbootmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.dogametal.springbootmongodb.domain.Post;
 //MongoRepository(Type first Entity and type of ID this case is String)
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase (String text);
 }
--- src/main/java/com/dogametal/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/dogametal/springbootmongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.dogametal.springbootmongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dogametal.springbootmongodb.domain.Post;
+import com.dogametal.springbootmongodb.resources.util.URL;
 import com.dogametal.springbootmongodb.services.PostService;
 
 @RestController
@@ -23,5 +27,11 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);  
 	}
-	
+
+	@RequestMapping(value ="/titlesearch", method = RequestMethod.GET) // Another way to use this request @GetMapping
+	public ResponseEntity <List<Post>> findbyTitle(@RequestParam(value="text", defaultValue = "") String text ){		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);  
+	}
 }
